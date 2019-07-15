69ffe7e6b8a25cdfdab8c3b0381fdb947d4fd776
Pavel Kalyugin <kalyuginpavel@gmail.com>
2019-07-15 14:09:04 +0300

    Initial commit
    Adds icons, pages and brief structure

---
+++ main.js
@@ -0,0 +1,62 @@
+const {app, Tray, Menu, BrowserWindow} = require('electron');
+const positioner = require('electron-traywindow-positioner');
+const path = require('path');
+const url = require('url');
+
+const trayIconPath = path.join(__dirname, './assets/trayIcon.png');
+const mainIconPath = path.join(__dirname, './assets/mainIcon.png');
+
+let tray;
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width:400,
+        height: 600,
+        frame: false,
+        show: false,
+        icon: mainIconPath
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'pages/tasks_list.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('blur', () => {
+        mainWindow.hide();
+    });
+    
+    tray = new Tray(trayIconPath);
+    tray.setToolTip("Taskodoro is live");
+    tray.setContextMenu(trayContextMenu);
+
+    tray.on('click', (_, bouds) => {
+        positioner.position(mainWindow, bouds);
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            mainWindow.show();
+        };
+    });    
+});
+
+var trayContextMenu = Menu.buildFromTemplate([
+    {
+        label: 'Toogle DevTools',
+        accelerator: 'Ctrl+I',
+        click() {
+            mainWindow.show();
+            mainWindow.toggleDevTools();
+        }
+    },
+    {
+        label: 'Quit',
+        accelerator: 'Ctrl+Q',
+        click() {
+            app.quit();
+        }
+    }
+]);
+
